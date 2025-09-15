2d6614443082ed332663b42ee7e1817722c61f4e
Đậu Khoa <khoakomlem@gmail.com>
2025-09-15 23:20:05 +0700

    update global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,12 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+import { ErrorBoundary } from "react-error-boundary"
+import Providers from "./providers"
+import { Toaster } from "@/components/ui/sonner"
 
 export const metadata: Metadata = {
-	title: "ShopNexus - Your Trusted B2B Marketplace",
+	title: "ShopNexus - Buy and Sell Anything!",
 	description:
 		"Connect with verified suppliers and grow your business with confidence. Professional B2B ecommerce platform for bulk orders, office supplies, and business solutions.",
 }
@@ -18,8 +21,11 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-				{children}
+				<ErrorBoundary fallback={<div>Something went wrong</div>}>
+					<Providers>{children}</Providers>
+				</ErrorBoundary>
 				<Analytics />
+				<Toaster />
 			</body>
 		</html>
 	)
