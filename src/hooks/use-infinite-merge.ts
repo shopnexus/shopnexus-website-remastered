2291a8c59446ba4a9eaa683bb925107cccb99e75
Đậu Khoa <khoakomlem@gmail.com>
2025-09-29 23:41:57 +0700

    update use infinite merge

--- src/hooks/use-infinite-merge.ts
+++ src/hooks/use-infinite-merge.ts
@@ -1,16 +1,16 @@
-import { InfiniteData } from '@tanstack/react-query'
+import { SuccessPaginationRes } from '@/lib/queryclient/response.type'
+import { InfiniteData, UseInfiniteQueryResult } from '@tanstack/react-query'
 import { useMemo } from 'react'
 
-type ListData<T> = {
-  data: T[]
-}
-
-export function useInfiniteMerge<T>(
-  data: InfiniteData<ListData<T>> | undefined,
+export function useInfiniteMerge<Entity, TPageParams = unknown, TError = Error>(
+  infiniteResult: UseInfiniteQueryResult<
+    InfiniteData<SuccessPaginationRes<Entity>, TPageParams>,
+    TError
+  >,
 ) {
   const items = useMemo(
-    () => data?.pages.flatMap(page => page.data) ?? [],
-    [data?.pages],
+    () => infiniteResult?.data?.pages.flatMap(page => page.data) ?? [],
+    [infiniteResult?.data?.pages],
   )
 
   return items
